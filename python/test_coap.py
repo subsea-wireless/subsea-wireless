5da67c85a2031c4563467d6217c2f1d7b7f7b5ef
Nigel Orr <nigel.orr@imenco.com>
2026-06-25 16:39:27 +0100

    Shuffled and refactored to separate functions to be moved to common.py and test functions to demonstrate flow

--- python/test_coap.py
+++ python/test_coap.py
@@ -1,7 +1,12 @@
 import aiocoap
 from cobs import cobs
 
+
 def crc16_ccitt(data: bytes, crc: int = 0xFFFF) -> int:
+    """ Calculate the CRC16-CCITT checksum for the given data with CRC specifications:
+        polynomial 0x1021, initial value 0xFFFF, no final XOR, and no reflection.
+        Verified against https://www.codertools.net/tools/crc.php     
+    """
     poly = 0x1021
     for byte in data:
         crc ^= byte << 8
@@ -13,9 +18,57 @@ def crc16_ccitt(data: bytes, crc: int = 0xFFFF) -> int:
     return crc
 
 
-def append_crc16(data: bytes) -> bytes:
-    crc = crc16_ccitt(data)
-    return data + crc.to_bytes(2, "big")
+def get_udp_message_bytes(code, protobuf, mid, mtype):
+    """ Create a UDP message with the given code, protobuf, and message ID
+        as a CoAP message byte stream, with the provided parameters
+    """
+    message = aiocoap.Message(code=code, payload=protobuf)
+    message.mid = mid
+    message.mtype = mtype
+    return message.encode()
+
+
+def get_serial_message_bytes(code, protobuf, mid, mtype):
+    """ Create a serial message with the given code, protobuf, and message ID
+        by wrapping the base CoAP message with checksum, COBS encoding, and null delimiters
+    """
+    message_bytes = get_udp_message_bytes(code, protobuf, mid, mtype)
+    serial_crc = crc16_ccitt(message_bytes).to_bytes(2, "big")
+    serial_message = bytes([0x00]) + cobs.encode(message_bytes + serial_crc) + bytes([0x00])
+    return serial_message
+
+
+def coap_message_from_udp_bytes(message_bytes):
+    """ Extract the CoAP message from a UDP message"""
+    message = aiocoap.Message.decode(message_bytes)
+    return message
+
+
+def coap_message_from_serial_bytes(serial_message):
+    """ Extract the CoAP message from a serial message
+        removing serial-specific framing and using 
+        coap_message_from_udp_bytes to decode the CoAP message
+    """
+    # Remove any leading and trailing null COBS delimiter(s)
+    while serial_message.startswith(b'\x00') and len(serial_message) > 1:
+        serial_message = serial_message[1:]
+    while serial_message.endswith(b'\x00') and len(serial_message) > 1:
+        serial_message = serial_message[:-1]
+
+    if len(serial_message) > 3: # data + CRC
+        cobs_decoded = cobs.decode(serial_message)
+        message_bytes = cobs_decoded[:-2]  # Remove CRC
+        crc_received = int.from_bytes(cobs_decoded[-2:], "big")
+        crc_calculated = crc16_ccitt(message_bytes)
+        if crc_received == crc_calculated:
+            return coap_message_from_udp_bytes(message_bytes)
+        else:
+            raise ValueError("Serial CRC mismatch")
+    else:
+        raise ValueError("Serial message too short")
+    
+
+###### Test functions to demonstrate/test the above functions and perform typical message flows
 
 
 def crc_test(data: bytes):
@@ -33,62 +86,36 @@ def crc_test(data: bytes):
         print("CRC verification failed.")
 
 
-def request(code, payload, mid):
-    """ Send a GET request, payload is a SWiG COBS Protobuf message"""
-    request = aiocoap.Message(code=code, payload=payload)
-    request.mid = mid    # May want to manage with a Context
-    request.mtype = aiocoap.CON
-    print(f'Sending {request}')
-    return request
-
-
-def receive(message_bytes):
-    """ Receive a message from the wire, decode and parse it"""
-    message = aiocoap.Message.decode(message_bytes)
-    print(f'Received {message}')
-    payload = message.payload
-    return message
-
-
-def respond(message, payload=b''):
-    """ Assemble a response to the provided Message"""
-    response = aiocoap.Message()
-    if len(payload):
-        response.code = aiocoap.CONTENT
-    else:
-        response.code = aiocoap.EMPTY
-    response.mtype = aiocoap.ACK
-    response.mid = message.mid
-    response.payload = payload
-    print(f'Responding with {message}')
-    return response
-
-
-def run_GET_test():
+def run_GET_over_serial_test():
     print("GET example message flow")
 # Create a request
-    message = request(code=aiocoap.GET, payload=b'SWiG Protobuf request packet placeholder', mid=0x5347)
-    message_bytes = message.encode()    # Ready to send on the wire
-    print(f"UDP CoAP message: [{message_bytes}]")
+    message_id = 0x5347
+    payload = b'SWiG Protobuf request packet placeholder'
 
-    # Demonstrate what would be sent over serial
-    serial_crc = crc16_ccitt(message_bytes).to_bytes(2, "big")
-    serial_message = bytes([0x00]) + cobs.encode(message_bytes + serial_crc) + bytes([0x00])
+    # Prepare and 'send' serial bytes
+    serial_message = get_serial_message_bytes(aiocoap.GET, protobuf = payload, mid = message_id, mtype = aiocoap.CON)
     print(f"Serial message (Protobuf + CoAP + CRC + COBS + delimiters): [{serial_message}]")
     
-    message = receive(message_bytes)
+    # 'receive' and process serial bytes
+    message = coap_message_from_serial_bytes(serial_message)
 
     if message.code.is_request():
         print(f'Request- payload is {message.payload}')
     else:
         print(f'Not a request, code is {message.code}')
     
-# Respond to the request
-    message = respond(message, b'SWiG Protobuf response packet placeholder')
-    response_bytes = message.encode()   # Ready to send on the wire
+    
+    # Prepare and 'send' serial response
+    payload = b'SWiG Protobuf response packet placeholder'
+    if len(payload):
+        code = aiocoap.CONTENT
+    else:
+        code = aiocoap.EMPTY
+    response_bytes = get_serial_message_bytes(code = code, protobuf = payload, mid = message.mid, mtype = aiocoap.ACK)
     print(f"Serial response: [{response_bytes}]")
     
-    message = receive(response_bytes)
+    # 'receive' and process the response
+    message = coap_message_from_serial_bytes(response_bytes)
 
     if message.code.is_response():
         print(f'Response- payload is {message.payload}')
@@ -96,32 +123,35 @@ def run_GET_test():
         print(f'Not a response, code is {message.code}')
 
 
-def run_POST_test():
+def run_POST_over_UDP_test():
     print("\nPOST example message flow")
 # Create a request
-    message = request(code=aiocoap.POST, payload=b'SWiG Protobuf value setting placeholder', mid=0x5350)
-    message_bytes = message.encode()    # CoAP ready to send on UDP or wrap for serial
-    print(f"UDP message: [{message_bytes}]")
-    serial_crc = bytes([0x43, 0x52])
+    message_id = 0x5350
+    payload = b'SWiG Protobuf value setting placeholder'
 
-    # Demonstrate what would be sent over serial
-    serial_crc = crc16_ccitt(message_bytes).to_bytes(2, "big")
-    serial_message = bytes([0x00]) + cobs.encode(message_bytes + serial_crc) + bytes([0x00])
-    print(f"Serial message (Protobuf + CoAP + CRC + COBS + delimiters): [{serial_message}]")
-    
-    message = receive(message_bytes)
+    # Prepare and 'send' UDP bytes
+    message_bytes = get_udp_message_bytes(aiocoap.POST, protobuf = payload, mid = message_id, mtype = aiocoap.CON)
+    print(f"UDP message: [{message_bytes}]")
+
+    # 'receive' and process UDP bytes
+    message = coap_message_from_udp_bytes(message_bytes)
 
     if message.code.is_request():
         print(f'Request- payload is {message.payload}')
     else:
         print(f'Not a request, code is {message.code}')
     
-# Respond to the request
-    message = respond(message)  # ACK with no payload
-    response_bytes = message.encode()   # Ready to send on the wire
-    print(f"Serial response: [{response_bytes}]")
-    
-    message = receive(response_bytes)
+    # Prepare and 'send' UDP response
+    payload = b'SWiG Protobuf response packet placeholder'
+    if len(payload):
+        code = aiocoap.CONTENT
+    else:
+        code = aiocoap.EMPTY
+    response_bytes = get_udp_message_bytes(code = code, protobuf = payload, mid = message.mid, mtype = aiocoap.ACK)
+    print(f"UDP response: [{response_bytes}]")
+
+    # 'receive' and process the response
+    message = coap_message_from_udp_bytes(response_bytes)
 
     if message.code.is_response():
         print(f'Response- payload is {message.payload}')
@@ -134,8 +164,8 @@ def main():
     # crc_test(str.encode("CRC Test Packet"))
 
     print("SWiG CoAP message flow with aiocoap")
-    run_GET_test()  # Used to get a parameter or status value
-    run_POST_test() # Set a parameter value
+    run_GET_over_serial_test()  # Used to get a parameter or status value
+    run_POST_over_UDP_test() # Set a parameter value
 
 
 if __name__ == "__main__":
